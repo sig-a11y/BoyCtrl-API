9c8d289c3dddb15af3da4f303b420dd083a8130e
woclass <git@wo-class.cn>
2023-10-29 15:17:31 +0800

    保益三方语音API v1.5.rar
    Name: 保益三方语音API v1.5.rar
    Size: 291794 bytes (284 KiB)
    CRC32: 8EB60D66
    MD5: 2222E6568C8CF52A57182BE20A39B518
    SHA256: 960B3D0D61940FACC94632E5842319E9EA18DCF396C5D9C5DA66BD8A5E47C0D1

--- BoyCtrl.h
+++ BoyCtrl.h
@@ -1,7 +1,8 @@
 #pragma once
 
 // 朗读完成回调
-typedef void (__stdcall*BoyCtrlSpeakCompleteFunc)();
+// reason: 回调原因，1=朗读完成，2=被新的朗读打断，3=被停止调用打断
+typedef void (__stdcall*BoyCtrlSpeakCompleteFunc)(int reason);
 
 // 错误码，要查看具体错误原因请在初始化时传入日志输出路径
 typedef enum
@@ -31,27 +32,30 @@ BoyCtrlError __stdcall BoyCtrlInitializeU8(const char* logPath);
 
 // 朗读文本
 // text: 朗读的文本，UTF-16le编码
-// withSlave: false=使用读屏语音，true=使用独立语音
+// withSlave: false=使用读屏通道，true=使用独立通道
 // append: 是否排队朗读
-// onCompletion: 朗读完成回调，NULL=完成不需要通知，当使用读屏语音时该参数忽略
-BoyCtrlError __stdcall BoyCtrlSpeak(const wchar_t* text, bool withSlave, bool append, BoyCtrlSpeakCompleteFunc onCompletion);
+// allowBreak: 是否允许用户打断，使用读屏通道时该参数被忽略
+// onCompletion: 朗读完成回调，NULL=完成不需要通知，使用读屏通道时该参数被忽略
+BoyCtrlError __stdcall BoyCtrlSpeak(const wchar_t* text, bool withSlave, bool append, bool allowBreak, BoyCtrlSpeakCompleteFunc onCompletion);
 
 // 朗读文本(ANSI)
 // text: 朗读的文本，ANSI编码
-// withSlave: false=使用读屏语音，true=使用独立语音
+// withSlave: false=使用读屏通道，true=使用独立通道
 // append: 是否排队朗读
-// onCompletion: 朗读完成回调，NULL=完成不需要通知，当使用读屏语音时该参数忽略
-BoyCtrlError __stdcall BoyCtrlSpeakAnsi(const char* text, bool withSlave, bool append, BoyCtrlSpeakCompleteFunc onCompletion);
+// allowBreak: 是否允许用户打断，使用读屏通道时该参数被忽略
+// onCompletion: 朗读完成回调，NULL=完成不需要通知，当使用读屏通道时该参数被忽略
+BoyCtrlError __stdcall BoyCtrlSpeakAnsi(const char* text, bool withSlave, bool append, bool allowBreak, BoyCtrlSpeakCompleteFunc onCompletion);
 
 // 朗读文本(UTF-8)
 // text: 朗读的文本，UTF-8编码
-// withSlave: false=使用读屏语音，true=使用独立语音
+// withSlave: false=使用读屏通道，true=使用独立通道
 // append: 是否排队朗读
-// onCompletion: 朗读完成回调，NULL=完成不需要通知，当使用读屏语音时该参数忽略
-BoyCtrlError __stdcall BoyCtrlSpeakU8(const char* text, bool withSlave, bool append, BoyCtrlSpeakCompleteFunc onCompletion);
+// allowBreak: 是否允许用户打断，使用读屏通道时该参数被忽略
+// onCompletion: 朗读完成回调，NULL=完成不需要通知，当使用读屏通道时该参数被忽略
+BoyCtrlError __stdcall BoyCtrlSpeakU8(const char* text, bool withSlave, bool append, bool allowBreak, BoyCtrlSpeakCompleteFunc onCompletion);
 
 // 停止朗读
-// withSlave: false=停止读屏语音，true=停止独立语音
+// withSlave: false=停止读屏通道，true=停止独立通道
 BoyCtrlError __stdcall BoyCtrlStopSpeaking(bool withSlave);
 
 // 退出清理
