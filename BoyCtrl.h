ad7b900d3e489d5456a581590215658171258563
Chengyu HAN <cyhan.dev@outlook.com>
2025-08-21 21:10:56 +0800

    BoyCtrl_v1.8.rar @ 2025-08-21
    Name: BoyCtrl_v1.8.rar
    Size: 315138 bytes : 307 KiB
    CRC32: 90C61225
    CRC64: 1C0C9FE7F6CEA21E
    SHA256: 6de0c53882839b5b4746eca4eca9aa77c5aac14d76ab603993c75626ed4df935
    SHA1: 3023f5037694ffcf4b5ba9753d29dfee06eda58c
    BLAKE2sp: 6b43fffc792e36f1c441a2bf4befdce544696444ca255ce317abf38a0995ff8c
    MD5: 9c7a2aeb3cfd2102aef2f81153e020ca
    XXH64: AEB8548BD059505F
    SHA384: c4721ad3ee457dac23ce15945f82945c98f7b1695bbfe1a6d0cde5b7e2df000cbddc8684106ec6a5440e3ea5beb3eb01
    SHA512: 3370d79131ee34106a97254c89579848d6d2ec9ec9b86adfb4afb656fa5630b6444b6a2320cb3e816ff06ce46fa1cc98c28f93263e02b63dc82cf53c29576586
    SHA3-256: d88310a5092219bfe2c60219304494ac6435100d5d1a703cc4e7b2db4ce7624a

--- BoyCtrl.h
+++ BoyCtrl.h
@@ -2,7 +2,7 @@
 
 // 朗读完成回调
 // reason: 回调原因，1=朗读完成，2=被新的朗读打断，3=被停止调用打断
-typedef void (__stdcall*BoyCtrlSpeakCompleteFunc)(int reason);
+typedef void(__stdcall* BoyCtrlSpeakCompleteFunc)(int reason);
 
 // 错误码，要查看具体错误原因请在初始化时传入日志输出路径
 typedef enum
@@ -17,6 +17,22 @@ typedef enum
 	e_bcerr_unavailable = 3,
 } BoyCtrlError;
 
+// 朗读标志
+typedef enum
+{
+	e_bcspf_none = 0,
+	e_bcspf_withSlave = 1,
+	e_bcspf_append = 2,
+	e_bcspf_allowBreak = 4,
+	e_bcspf_isReader = 8,
+} BoyCtrlSpeakFlags;
+
+// 信息朗读模式
+typedef enum
+{
+	e_bcirm_time,
+	e_bcirm_date,
+} BoyCtrlInfoReportMode;
 
 // 初始化接口
 // logPath: 日志输出路径，UTF-16le编码，NULL或空字符串=无日志输出
@@ -38,6 +54,12 @@ BoyCtrlError __stdcall BoyCtrlInitializeU8(const char* logPath);
 // onCompletion: 朗读完成回调，NULL=完成不需要通知，使用读屏通道时该参数被忽略
 BoyCtrlError __stdcall BoyCtrlSpeak(const wchar_t* text, bool withSlave, bool append, bool allowBreak, BoyCtrlSpeakCompleteFunc onCompletion);
 
+// 朗读文本
+// text: 朗读的文本，UTF-16le编码
+// flags: 朗读标志，参考BoyCtrlSpeakFlags
+// onCompletion: 朗读完成回调，NULL=完成不需要通知，使用读屏通道时该参数被忽略
+BoyCtrlError __stdcall BoyCtrlSpeakEx(const wchar_t* text, int flags, BoyCtrlSpeakCompleteFunc onCompletion);
+
 // 朗读文本(ANSI)
 // text: 朗读的文本，ANSI编码
 // withSlave: false=使用读屏通道，true=使用独立通道
@@ -58,6 +80,10 @@ BoyCtrlError __stdcall BoyCtrlSpeakU8(const char* text, bool withSlave, bool app
 // withSlave: false=停止读屏通道，true=停止独立通道
 BoyCtrlError __stdcall BoyCtrlStopSpeaking(bool withSlave);
 
+// 停止朗读
+// flags: 功能标志，参考BoyCtrlSpeakFlags
+BoyCtrlError __stdcall BoyCtrlStopSpeakingEx(int flags);
+
 // 暂停指定时间的读屏朗读
 // ms: 暂停时间，单位毫秒，10~10000ms
 BoyCtrlError __stdcall BoyCtrlPauseScreenReader(int ms);
@@ -68,8 +94,17 @@ void __stdcall BoyCtrlUninitialize();
 // 读屏是否正在运行，必须成功初始化才能使用此接口
 bool __stdcall BoyCtrlIsReaderRunning();
 
+// 获取读屏状态(0=未运行，1=正在运行但功能受限，2=正在运行且功能无限制)，
+// 必须成功初始化才能使用此接口
+int __stdcall BoyCtrlGetReaderState();
+
 // 验证
 bool __stdcall BoyCtrlVerify(const char* key);
 
 // 设置任意键打断，必须成功初始化后才能使用此接口
+// withSlave: false=设置读屏通道，true=设置独立通道
 bool __stdcall BoyCtrlSetAnyKeyStopSpeaking(bool withSlave);
+
+// 读屏信息朗读，必须成功初始化后才能使用此接口
+// mode: 朗读模式，参考BoyCtrlInfoReportMode
+bool __stdcall BoyCtrlReportInfo(int mode);
